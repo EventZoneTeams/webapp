0f7742b4908ccd2d86ce118611da832d11bff830
hnam-truong <truonghahaonam@gmail.com>
2024-05-16 12:00:21 +0700

    set up source

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from '@/components/theme/theme-toggle';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className=''>
+      <ModeToggle />
+    </main>
+  );
+}
