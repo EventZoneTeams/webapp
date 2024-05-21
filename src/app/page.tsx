cea1504af90f1992a733f31c00871f3bf090735d
ngogiahuan <ngohuan122003@gmail.com>
2024-05-21 14:32:16 +0700

    add all component from shadcn, add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { ModeToggle } from '@/components/theme/theme-toggle';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <main className=''>
-      <ModeToggle />
+      <Header />
     </main>
   );
 }
