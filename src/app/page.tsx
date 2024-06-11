9b93d626b6d609bd9546914b4062610b520a0dba
hnam-truong <truonghahaonam@gmail.com>
2024-06-11 13:01:11 +0700

    carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function Home() {
-  return <main className=" ">home</main>;
+  return (
+    <main className="">
+      <div className="flex justify-center">
+        <Carousel className="">
+          <CarouselContent className="w-[1000px] h-[400px]">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card className="flex justify-center items-center">
+                    <CardContent className="flex aspect-square items-center justify-center p-6 w-[1000px] h-[400px]">
+                      <img
+                        className="w-[1000px] h-[400px] object-cover"
+                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+                      />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </main>
+  );
 }
