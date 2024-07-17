34635b5696a512f4a63d05fb5a7417524b897153
ngogiahuan <ngohuan122003@gmail.com>
2024-07-18 01:54:26 +0700

    complete api function for event order

--- src/app/dashboard/@organizer/my-events/[id]/orders/layout.tsx
+++ src/app/dashboard/@organizer/my-events/[id]/orders/layout.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Orders",
+};
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return children;
