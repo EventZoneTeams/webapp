dcde06e9be04b221ecb241e3b71e6556754434b7
ngogiahuan <ngohuan122003@gmail.com>
2024-07-19 11:21:22 +0700

    at table for user manage

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,20 +1,31 @@
+import { GetUserSendData } from "@/api/user";
 import { User } from "@/types/authuser";
 import { create } from "zustand";
 
 interface UserStore {
+  trigger: boolean;
+  switchTrigger: (trigger: boolean) => void;
+  queryObj: GetUserSendData;
+  setQueryObj: (queryObj: GetUserSendData) => void;
   authUser: User | null;
   setAuthUser: (authUser: User) => void;
   reset: () => void;
 }
 
 const initialState: UserStore = {
+  trigger: false,
+  switchTrigger: () => {},
+  queryObj: {},
+  setQueryObj: () => {},
   authUser: null,
   setAuthUser: () => {},
   reset: () => {},
 };
 
 export const useUserStore = create<UserStore>()((set) => ({
-  authUser: null,
+  ...initialState,
+  switchTrigger: (trigger) => set({ trigger }),
+  setQueryObj: (queryObj) => set({ queryObj }),
   setAuthUser: (authUser) => set({ authUser }),
   reset: () => set(initialState),
 }));
