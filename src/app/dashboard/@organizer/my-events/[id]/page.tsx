bd9018e9928c4e00a051176ed97d29b3c4ea5856
ngogiahuan <ngohuan122003@gmail.com>
2024-07-19 10:52:30 +0700

    fix dashboard

--- src/app/dashboard/@organizer/my-events/[id]/page.tsx
+++ src/app/dashboard/@organizer/my-events/[id]/page.tsx
@@ -5,10 +5,13 @@ import DashboardCardList from "@/components/DashboardCard/DashboardCardList";
 import { AreaChartStacked } from "@/components/Example/Charts/AreaChartStacked";
 import { PieChartComponent } from "@/components/Example/Charts/PieChart";
 import useEvent from "@/hooks/useEvent";
+import useEventOrder from "@/hooks/useEventOrder";
+import useEventPackage from "@/hooks/useEventPackage";
+import useEventProduct from "@/hooks/useEventProduct";
 import { Coins, Package } from "lucide-react";
 import { useEffect, useMemo } from "react";
 
-const cards: DashboardCardProps[] = [
+let cards: DashboardCardProps[] = [
   {
     title: "Total Products",
     value: 0,
@@ -27,9 +30,33 @@ const cards: DashboardCardProps[] = [
 
 export default function page({ params }: { params: { id: string } }) {
   const { getEventByIdMutation } = useEvent();
+  const { products, setQueryObj: setQueryObjProduct } = useEventProduct();
+  const { eventPackages, setQueryObj: setQueryObjPackage } = useEventPackage();
+  const { orders, setQueryObj: setQueryObjOrder } = useEventOrder();
   useEffect(() => {
     getEventByIdMutation.mutate(Number(params.id));
+    setQueryObjProduct({ EventId: Number(params.id) });
+    setQueryObjPackage({ EventId: Number(params.id) });
+    setQueryObjOrder({ id: Number(params.id) });
   }, [params.id]);
+
+  useEffect(() => {
+    if (products) {
+      cards[0].value = products.length;
+    }
+  }, [products]);
+
+  useEffect(() => {
+    if (eventPackages) {
+      cards[1].value = eventPackages.length;
+    }
+  }, [eventPackages]);
+
+  useEffect(() => {
+    if (orders) {
+      cards[2].value = orders.length;
+    }
+  }, [orders]);
 
   return (
     <div className="py-4 space-y-4">
