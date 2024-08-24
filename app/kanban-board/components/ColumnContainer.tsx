071e686ff0fe3db4ada5a0943ea3abd5349e2a6b
hnam-truong <truonghahaonam@gmail.com>
2024-08-24 14:06:02 +0700

    columns

---
+++ app/kanban-board/components/ColumnContainer.tsx
@@ -0,0 +1,158 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EventBoardColumn } from "@/types/eventBoard";
+import { useSortable } from "@dnd-kit/sortable";
+import { Ellipsis, Trash2 } from "lucide-react";
+import { CSS } from "@dnd-kit/utilities";
+
+interface ColumnContainerProps {
+  column: EventBoardColumn;
+  deleteColumn: (id: string) => void;
+}
+
+function ColumnContainer(props: ColumnContainerProps) {
+  const { column, deleteColumn } = props;
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="flex h-[500px] max-h-[500px] w-[16rem] flex-col rounded-md border border-primary bg-white px-4 py-2 opacity-50"
+      >
+        {/* Column title */}
+        <section
+          {...attributes}
+          {...listeners}
+          className="flex min-h-[2.5rem] items-start justify-between"
+        >
+          <div className="flex items-start gap-2">
+            <div className="flex h-[2rem] items-center">
+              <p className="flex h-6 w-6 items-start justify-center rounded-sm bg-gray-200 text-xs"></p>
+            </div>
+            <div className="mt-1">
+              <p className="flex items-start font-semibold">{column.name}</p>
+            </div>
+          </div>
+
+          <div className="flex h-[2rem] items-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="h-8 px-2" variant="ghost">
+                  <Ellipsis size={20} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>List actions</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem
+                  onClick={() => deleteColumn(column.id)}
+                  className="cursor-pointer gap-2"
+                >
+                  <Trash2 size={20} />
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </section>
+
+        <hr />
+
+        {/* Column task container */}
+        <section>
+          <div className="flex flex-grow">Content</div>
+        </section>
+
+        {/* Column footer */}
+        <section>
+          <div>Footer</div>
+        </section>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex h-[500px] max-h-[500px] w-[16rem] flex-col rounded-md border bg-white px-4 py-2"
+    >
+      {/* Column title */}
+      <section className="flex min-h-[2.5rem] items-start justify-between">
+        <div {...attributes} {...listeners} className="flex items-start gap-2 w-full">
+          <div className="flex h-[2rem] items-center">
+            <p className="flex h-6 w-6 items-start justify-center rounded-sm bg-gray-200 text-xs"></p>
+          </div>
+          <div className="mt-1">
+            <p className="flex items-start font-semibold">{column.name}</p>
+          </div>
+        </div>
+
+        <div className="flex h-[2rem] items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="h-8 px-2" variant="ghost">
+                <Ellipsis size={20} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>List actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem
+                onClick={() => deleteColumn(column.id)}
+                className="cursor-pointer gap-2"
+              >
+                <Trash2 size={20} />
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </section>
+
+      <hr />
+
+      {/* Column task container */}
+      <section>
+        <div className="flex flex-grow">Content</div>
+      </section>
+
+      {/* Column footer */}
+      <section>
+        <div>Footer</div>
+      </section>
+    </div>
+  );
+}
+
+export default ColumnContainer;
---
+++ app/kanban-board/components/KanbanBoard.tsx
@@ -0,0 +1,125 @@
+import { Button } from "@/components/ui/button";
+import { EventBoardColumn } from "@/types/eventBoard";
+import { Plus } from "lucide-react";
+import { useMemo, useState } from "react";
+import ColumnContainer from "./ColumnContainer";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+
+function KanbanBoard() {
+  const [columns, setColumns] = useState<EventBoardColumn[]>([]);
+  const columnsId = useMemo(
+    () => columns.map((column) => column.id),
+    [columns],
+  );
+  const [activeColumn, setActiveColumn] = useState<EventBoardColumn | null>(
+    null,
+  );
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    }),
+  );
+
+  console.log(columns);
+
+  const createColumn = () => {
+    const columnToAdd: EventBoardColumn = {
+      id: Math.random().toString(36).substring(7),
+      name: `Column ${columns.length + 1}`,
+      color: null,
+      eventBoardTasks: [],
+    };
+    setColumns([...columns, columnToAdd]);
+  };
+
+  const deleteColumn = (id: string) => {
+    const filteredColumns = columns.filter((column) => column.id !== id);
+    setColumns(filteredColumns);
+  };
+
+  const onDragStart = (event: DragStartEvent) => {
+    console.log(event);
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  };
+
+  const onDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over) return;
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId !== overColumnId) {
+      setColumns((columns) => {
+        const activeColumnIndex = columns.findIndex(
+          (column) => column.id === activeColumnId,
+        );
+        const overColumnIndex = columns.findIndex(
+          (column) => column.id === overColumnId,
+        );
+
+        return arrayMove(columns, activeColumnIndex, overColumnIndex);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className="ml-10 mt-10 flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((column) => (
+                <div key={column.id}>
+                  <ColumnContainer
+                    column={column}
+                    deleteColumn={deleteColumn}
+                  />
+                </div>
+              ))}
+            </SortableContext>
+          </div>
+          <Button
+            className="flex w-[16rem] items-center justify-start gap-2"
+            variant={"outline"}
+            onClick={createColumn}
+          >
+            <Plus size={20} />
+            Add another list
+          </Button>
+        </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body,
+        )}
+      </DndContext>
+    </div>
+  );
+}
+
+export default KanbanBoard;
