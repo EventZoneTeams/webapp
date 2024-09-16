36cac87b16793f0c54af1485b0ece82122b2cab2
ngogiahuandev <ngogiahuan23122003@gmail.com>
2024-09-16 14:15:38 +0700

    fix run build

--- components/input/DateTimePicker.tsx
+++ components/input/DateTimePicker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button, buttonVariants } from "@/components/ui/button";
 import type { CalendarProps } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
