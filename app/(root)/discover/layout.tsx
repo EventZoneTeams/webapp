82e5bc0e5717ea46ea1996d2e07162525c960c9f
Ngô Gia Huấn <ngogiahuan23122003@gmail.com>
2024-09-27 13:49:45 +0700

    fix build error before merge

--- app/(root)/discover/layout.tsx
+++ app/(root)/discover/layout.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/shared/Header";
+import { Suspense } from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <Suspense>
       <Header />
       <div className="m-auto mt-12 w-[820px] py-4">{children}</div>
-    </div>
+    </Suspense>
   );
 }
